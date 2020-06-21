59bd902227900107aded878cd7a911fb98be3ce5
Stevan Bogosavljevic <stevyhacker@gmail.com>
2020-06-21 18:54:52 +0200

    Missing scene files added

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float restartDelay = 1f;
+
+    public void CompleteLevel ()
+    {
+        Debug.Log("Next Level");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Game Over");
+        Invoke("Restart", restartDelay);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
