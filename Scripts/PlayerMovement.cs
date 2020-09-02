dce50ee0d8a7e47c7e9e7fdbf531e7ef5c9a193e
Stevan Bogosavljevic <stevyhacker@gmail.com>
2020-09-02 13:20:52 +0200

    Mobile joystick controller support implemented

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -10,8 +10,11 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public Rigidbody rb;
+    public Joystick joystick;
     public float forwardForceMovement = 1000f;
     public float sidewaysForceMovement = 1000f;
+    public float speed;
+
 
     // Update is called once per frame
     void Update()
@@ -25,28 +28,32 @@ public class PlayerMovement : MonoBehaviour
 
         // If the player is pressing the d or a keys
 
-        if (Input.GetKey("d") || Input.GetKey("right"))
+        if (Input.GetKey("d") || Input.GetKey("right") || joystick.Horizontal > 0.5f)
         {
             // Add a force to the right
             // Debug.Log("Right arrow key was pressed.");
             rb.AddForce(0, 0, sidewaysForceMovement * Time.deltaTime);
         }
 
-        if (Input.GetKey("a") || Input.GetKey("left"))
+        if (Input.GetKey("a") || Input.GetKey("left") || joystick.Horizontal < -0.5f)
         {
             // Add a force to the left
-            // Debug.Log("Left arrow key was pressed.");
+            // Debug.Log("Left arrow key was pressed.");z
             rb.AddForce(0, 0, -sidewaysForceMovement * Time.deltaTime);
         }
 
-        if (Input.GetKey("s") || Input.GetKey("down"))
+        if (Input.GetKey("s") || Input.GetKey("down") ||  joystick.Vertical < -0.5f )
         {
             rb.AddForce(forwardForceMovement * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey("w") || Input.GetKey("up"))
+        if (Input.GetKey("w") || Input.GetKey("up") || joystick.Vertical > 0.5f)
         {
             rb.AddForce(-forwardForceMovement * Time.deltaTime, 0, 0);
         }
+
+        // Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.left * joystick.Horizontal;
+        // rb.AddForce(direction * (speed * Time.fixedDeltaTime));
+
     }
 }
